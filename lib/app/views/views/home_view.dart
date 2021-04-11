73a6e89f7dcb78568bd87e43acc791e9ce67c41c
İbrahim YILDIZ <yildizib@gmail.com>
2021-04-11 03:17:47 +0300

    locale test ...

--- lib/app/views/views/home_view.dart
+++ lib/app/views/views/home_view.dart
@@ -2,19 +2,29 @@ import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
 import 'package:news_app/app/controllers/home_controller.dart';
+import 'package:news_app/app/locales/locales.g.dart';
 
 class HomeView extends GetView<HomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('HomeView'),
+        title: Text('buttons_login'.tr),
         centerTitle: true,
       ),
       body: Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
+        child: Column(
+          children: [
+            Text(
+              'buttons_login'.tr,
+              style: TextStyle(fontSize: 20),
+            ),
+            IconButton(
+                icon: Icon(Icons.ac_unit),
+                onPressed: () {
+                  Get.updateLocale(Locale('en', 'US'));
+                })
+          ],
         ),
       ),
     );
