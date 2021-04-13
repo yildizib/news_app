1bc4351c3cadc0da1b608ed84c1186ae0a3864df
İbrahim YILDIZ <yildizib@gmail.com>
2021-04-13 23:40:12 +0300

    ...

---
+++ lib/app/configs/provider_config.dart
@@ -0,0 +1,3 @@
+class ProviderConfig {
+  static const apiUrl = 'http://localhost:8080/api/v1';
+}
--- lib/app/models/providers/category_provider.dart
+++ lib/app/models/providers/category_provider.dart
@@ -1,4 +1,5 @@
 import 'package:get/get.dart';
+import 'package:news_app/app/configs/provider_config.dart';
 
 import '../category_model.dart';
 
@@ -6,9 +7,11 @@ class CategoryProvider extends GetConnect {
   @override
   void onInit() {
     httpClient.defaultDecoder = (map) => Category.fromJson(map);
-    httpClient.baseUrl = 'YOUR-API-URL';
+    httpClient.baseUrl = ProviderConfig.apiUrl + '/category';
   }
 
+  Future<Response<List<Category>>> getCategories() async =>
+      await get('category');
   Future<Response<Category>> getCategory(int id) async =>
       await get('category/$id');
   Future<Response<Category>> postCategory(Category category) async =>
